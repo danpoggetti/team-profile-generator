9bb1b911c5b8dd547b6e39e5a6a1189e6a729b3d
danpoggetti <danpoggetti@gmail.com>
2022-12-01 10:22:24 -0800

    added engineer info to index.js

--- index.js
+++ index.js
@@ -74,4 +74,88 @@ function appMenu() {
             createTeam();
         });
     }
+
+
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of member do you want to add?",
+                choices: ["Engineer", "Intern", "I actually do not want to add more members."]
+            }
+        ]).then(userChoice => {
+            switch (userChoice.memberChoice) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                    case "Intern":
+                        addIntern();
+                        break;
+                        default:
+                            buildTeam();
+            }
+        });
+    }
+
+    function createEngineer() {
+        console.log("Please create your team!")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the name of the Engineer?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character!";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerID",
+                message: "What is the ID of the Engineer?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a positive number that is greater than zero!";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the email of the team manager?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\$+@\$+\.\$+/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email address!";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the Github for the Engineer?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character!";
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idArray.push(answers.engineerId);
+            createTeam();
+        });
+    }
 }
