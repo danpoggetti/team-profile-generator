badb7697d7e9395cec4aa036f578a682c16ef0a2
danpoggetti <danpoggetti@gmail.com>
2022-11-30 23:04:40 -0800

    added pageTemplate info to src

---
+++ src/pageTemplate.js
@@ -0,0 +1,92 @@
+const generateTeam = team => {
+    const generateManager = manager => {
+        return `
+        <div class= "card employee-card">
+            <div class= "card-header">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: ${manager.getEmail()}</li>
+                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                 </ul>
+            </div>
+        </div
+    `
+}
+
+    const generateEngineer = engineer => {
+        return `
+            <div class= "card employee-card">
+                <div class= "card-header">
+                    <h2 class="card-title">${engineer.getName()}</h2>
+                    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${engineer.getId()}</li>
+                        <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+                        <li class="list-group-item">Office number: ${engineer.getGithub()}</li>
+                    </ul>
+                </div>
+            </div
+        `
+    }
+
+    const generateIntern = intern => {
+        return `
+            <div class= "card employee-card">
+                <div class= "card-header">
+                    <h2 class="card-title">${intern.getName()}</h2>
+                    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">ID: ${intern.getId()}</li>
+                        <li class="list-group-item">Email: ${intern.getEmail()}</li>
+                        <li class="list-group-item">Office number: ${intern.getSchool()}</li>
+                    </ul>
+                </div>
+            </div
+        `
+    }
+}
+
+// lets export function to generate a page
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha3840ggOyR0iXCbMQv3Xipma34MD_dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                    ${generateTeam(team)}
+                </div>
+            </div>
+        </div>
+    
+    </body>
+    </html>
+    
+    
+    `
+};
